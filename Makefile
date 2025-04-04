73a91f99320aa9e373b8293e09e8abc645cca49f
Nick Bernard <nick.bernard@hey.com>
2025-04-03 21:15:25 -0700

    Add the register command

--- Makefile
+++ Makefile
@@ -1,4 +1,18 @@
 build:
 	go build -o bin/gator main.go
+
 run:
 	go run main.go
+
+migrate-up:
+	goose -dir sql/schema postgres "postgres://nick.bernard:@localhost:5432/gator" up
+
+migrate-down:
+	goose -dir sql/schema postgres "postgres://nick.bernard:@localhost:5432/gator" down
+
+reset:
+	$(MAKE) migrate-down
+	$(MAKE) migrate-up
+
+generate:
+	sqlc generate
--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"os"
 
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 	"github.com/nrbernard/gator/internal/config"
+	"github.com/nrbernard/gator/internal/database"
 )
 
 type state struct {
+	db     *database.Queries
 	config *config.Config
 }
 
@@ -37,11 +43,38 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	username := cmd.args[0]
+
+	if _, err := s.db.GetUser(context.Background(), username); err != nil {
+		return fmt.Errorf("user not found: %s", username)
+	}
+
 	if err := s.config.SetUser(username); err != nil {
-		return err
+		return fmt.Errorf("failed to set user: %s", err)
 	}
 
 	fmt.Println("User set to:", username)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("username required")
+	}
+
+	username := cmd.args[0]
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:   uuid.New(),
+		Name: username,
+	})
+	if err != nil {
+		fmt.Printf("failed to create user: %s\n", err)
+		os.Exit(1)
+	}
+
+	s.config.SetUser(username)
+
+	fmt.Printf("User created: %+v\n", user)
 	return nil
 }
 
@@ -52,7 +85,16 @@ func main() {
 		os.Exit(1)
 	}
 
+	db, err := sql.Open("postgres", configFile.DBUrl)
+	if err != nil {
+		fmt.Printf("Failed to connect to database: %s\n", err)
+		os.Exit(1)
+	}
+
+	dbQueries := database.New(db)
+
 	appState := &state{
+		db:     dbQueries,
 		config: configFile,
 	}
 
@@ -61,6 +103,7 @@ func main() {
 	}
 
 	commands.register("login", handlerLogin)
+	commands.register("register", handlerRegister)
 
 	args := os.Args[1:]
 	if len(args) == 0 {
